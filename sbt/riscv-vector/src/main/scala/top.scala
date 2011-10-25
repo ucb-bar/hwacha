45eb45aeeece4746cdd5f7cb81c8c15572ac1234
Daiwei Li <daiweili@gmail.com>
2011-10-25 03:11:13 -0700

    BHWD compiles and passes tests, tmp top module, src simlink

---
+++ sbt/riscv-vector/src/main/scala/top.scala
@@ -0,0 +1,26 @@
+package riscvVector {
+    import Chisel._
+    import Node._
+
+    /*
+    class TopIo extends Bundle {
+        val io = Bool('input); // Here temporarily just so this will compile
+    }
+
+    class Top extends Component {
+        override val io = new TopIo();
+        val vuVMU_BHWDsel = new vuVMU_BHWD_sel();
+        val vuVMU_Ctrl_ut_issue = new vuVMU_Ctrl_ut_issue();
+    }
+    */
+
+    object top_main {
+        def main(args: Array[String]) = {
+            val boot_args = args ++ Array("--target-dir", "generated-src");
+            chiselMain(
+                boot_args,
+                () => new vuVMU_Ctrl_ut_issue()
+            );
+    } 
+}
+}  
--- sbt/riscv-vector/src/main/scala/vuVMU-BHWDsel.scala
+++ sbt/riscv-vector/src/main/scala/vuVMU-BHWDsel.scala
@@ -5,30 +5,32 @@ package riscvVector {
 // byte/halfword/word/doubleword select + sign extension logic
 
 class vuVMU_BHWD_selIO extends Bundle {
-  val bhwd_sel		= UFix(2, 'input);
+  val bhwd_sel		= Bits(2, 'input);
   val signext		  = Bool('input);
-  val addr_lsb		= UFix(4, 'input);
-  val din		      = UFix(128, 'input);
-  val dout		    = UFix(64, 'output);
+  val addr_lsb		= Bits(4, 'input);
+  val din		      = Bits(128, 'input);
+  val dout		    = Bits(64, 'output);
 }
 
 class vuVMU_BHWD_sel extends Component {
   val io = new vuVMU_BHWD_selIO();
 
-  val dword_sel     = Mux(addr_lsb(3), io.din(127,64), io.din(63,0));
+  val dword_sel     = Mux(io.addr_lsb(3)===Bits(1,1), io.din(127,64), io.din(63,0));
 
-  val word_sel      = Mux(addr_lsb(2), dword_sel(63,32), dword_sel(31,0));
-  val word_sel_ext  = Mux(io.signext, Cat(UFix(0, 32), word_sel), Cat(Fill(32, word_sel(31)), word_sel));
+  val word_sel      = Mux(io.addr_lsb(2)===Bits(1,1), dword_sel(63,32), dword_sel(31,0));
+  val word_sel_ext  = Mux(io.signext, Cat(Bits(0, 32), word_sel), Cat(Fill(32, word_sel(31)), word_sel));
 
-  val hw_sel        = Mux(addr_lsb(1), word_sel(31,16), word_sel(15,0));
-  val hw_sel_ext    = Mux(io.signext, Cat(UFix(0, 48), hw_sel), Cat(Fill(48, hw_sel(15)), hw_sel));
+  val hw_sel        = Mux(io.addr_lsb(1)===Bits(1,1), word_sel(31,16), word_sel(15,0));
+  val hw_sel_ext    = Mux(io.signext, Cat(Bits(0, 48), hw_sel), Cat(Fill(48, hw_sel(15)), hw_sel));
   
-  val byte_sel      = Mux(addr_lsb(1), hw_sel(15,8), hw_sel(7,0));
-  val byte_sel_ext  = Mux(io.signext, Cat(UFix(0, 56), byte_sel), Cat(Fill(56, byte_sel(7)), hw_sel));
+  val byte_sel      = Mux(io.addr_lsb(0)===Bits(1,1), hw_sel(15,8), hw_sel(7,0));
+  val byte_sel_ext  = Mux(io.signext, Cat(Bits(0, 56), byte_sel), Cat(Fill(56, byte_sel(7)), byte_sel));
 
-  io.dout           := MuxLookup(io.bhwd_sel, UFix(0, 64), Array(
-    UFix(0, 2) -> byte_sel_ext,
-    UFix(1, 2) -> hw_sel_ext,
-    UFix(2, 2) -> word_sel_ext,
-    UFix(3, 2) -> dword_sel_ext));
+  io.dout           := MuxLookup(io.bhwd_sel, Bits(0, 64), Array(
+    Bits(0, 2) -> byte_sel_ext,
+    Bits(1, 2) -> hw_sel_ext,
+    Bits(2, 2) -> word_sel_ext,
+    Bits(3, 2) -> dword_sel));
+}
+
 }
