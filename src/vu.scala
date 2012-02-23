3e31d4aead1bcf00dfc1c7a0bac252f7db5cb40b
Yunsup Lee <yunsup@cs.berkeley.edu>
2012-02-23 02:53:35 -0800

    cleanup top level vu

--- src/vu.scala
+++ src/vu.scala
@@ -35,14 +35,6 @@ class vu extends Component
 
   val vmu_utcmdq = VC_SIMPLE_QUEUE(UTMCMD_SZ, 16)
   val vmu_utimmq = VC_SIMPLE_QUEUE(UTMIMM_SZ, 16)
-  val utaq = VC_SIMPLE_QUEUE(SZ_ADDR, 16)
-  val utaq_count = new vuVMU_QueueCount(16, 9, 16, true)
-
-  val utldq = VC_SIMPLE_QUEUE(SZ_DATA, 16)
-  val utldq_count = new vuVMU_QueueCount(0, 9, 16, true)
-
-  val utsdq = VC_SIMPLE_QUEUE(SZ_DATA, 16)
-  val utsdq_count = new vuVMU_QueueCount(16, 9, 16, true)
 
   // vxu
   io.illegal <> vxu.io.illegal
@@ -126,92 +118,4 @@ class vu extends Component
   vsackcnt.io.dec := vxu.io.lane_vsdq.valid && vsdq.io.enq.ready
   vsackcnt.io.qcnt := vxu.io.qcnt
   vxu.io.pending_store := !vsackcnt.io.zero
-
-  //val vmu = new vuVMU()
-
-  //// utaq
-  //vxu.io.lane_utaq.ready := utaq_count.io.watermark // utaq.io.enq.ready
-  //utaq.io.enq.valid := vxu.io.lane_utaq.valid
-  //utaq.io.enq.bits := vxu.io.lane_utaq.bits
-
-  //utaq.io.deq.ready := vmu.io.utaq_deq_ready
-  //vmu.io.utaq_deq_valid := utaq.io.deq.valid
-  //vmu.io.utaq_deq_bits := utaq.io.deq.bits
-
-  //utaq_count.io.qcnt := vxu.io.qcnt
-  //utaq_count.io.inc := vmu.io.utaq_deq_ready && utaq.io.deq.valid
-  //utaq_count.io.dec := vxu.io.lane_utaq.valid
-
-
-  //// utldq
-  //vmu.io.utldq_enq_ready := utldq.io.enq.ready
-  //utldq.io.enq.valid := vmu.io.utldq_enq_valid
-  //utldq.io.enq.bits := vmu.io.utldq_enq_bits
-
-  //utldq.io.deq.ready := vxu.io.lane_utldq.ready
-  //vxu.io.lane_utldq.valid := utldq_count.io.watermark // utldq.deq.valid
-  //vxu.io.lane_utldq.bits := utldq.io.deq.bits
-
-  //utldq_count.io.qcnt := vxu.io.qcnt
-  //utldq_count.io.inc := utldq.io.enq.ready && vmu.io.utldq_enq_valid
-  //utldq_count.io.dec := vxu.io.lane_utldq.ready
-
-
-  //// utsdq
-  //vxu.io.lane_utsdq.ready := utsdq_count.io.watermark // utsdq.io.enq.ready
-  //utsdq.io.enq.valid := vxu.io.lane_utsdq.valid
-  //utsdq.io.enq.bits := vxu.io.lane_utsdq.bits
-
-  //utsdq.io.deq.ready := vmu.io.utsdq_deq_ready
-  //vmu.io.utsdq_deq_valid := utsdq.io.deq.valid
-  //vmu.io.utsdq_deq_bits := utsdq.io.deq.bits
-
-  //utsdq_count.io.qcnt := vxu.io.qcnt
-  //utsdq_count.io.inc := vmu.io.utsdq_deq_ready && utsdq.io.deq.valid
-  //utsdq_count.io.dec := vxu.io.lane_utsdq.valid
-
-
-  //// vmu
-  //vmu.io.vmu_vcmdq.bits <> vmu_vcmdq.io.deq.bits
-  //vmu.io.vmu_vcmdq.valid <> vmu_vcmdq.io.deq.valid
-  //vmu.io.vmu_vcmdq.rdy <> vmu_vcmdq.io.deq.ready
-
-  //vmu.io.vmu_vbaseq.bits <> vmu_vbaseq.io.deq.bits
-  //vmu.io.vmu_vbaseq.valid <> vmu_vbaseq.io.deq.valid
-  //vmu.io.vmu_vbaseq.rdy <> vmu_vbaseq.io.deq.ready
-  //
-  //vmu.io.vmu_vstrideq.bits <> vmu_vstrideq.io.deq.bits
-  //vmu.io.vmu_vstrideq.valid <> vmu_vstrideq.io.deq.valid
-  //vmu.io.vmu_vstrideq.rdy <> vmu_vstrideq.io.deq.ready
-
-  //vmu.io.vmu_utcmdq.bits <> vmu_utcmdq.io.deq.bits
-  //vmu.io.vmu_utcmdq.valid <> vmu_utcmdq.io.deq.valid
-  //vmu.io.vmu_utcmdq.rdy <> vmu_utcmdq.io.deq.ready
-
-  //vmu.io.vmu_utimmq.bits <> vmu_utimmq.io.deq.bits
-  //vmu.io.vmu_utimmq.valid <> vmu_utimmq.io.deq.valid
-  //vmu.io.vmu_utimmq.rdy <> vmu_utimmq.io.deq.ready
-
-  //vmu.io.dmem_req_vec.addr <> io.dmem_req_vec.bits.addr
-  //vmu.io.dmem_req_vec.op <> io.dmem_req_vec.bits.op
-  //vmu.io.dmem_req_vec.data <> io.dmem_req_vec.bits.data
-  //vmu.io.dmem_req_vec.wmask <> io.dmem_req_vec.bits.wmask
-  //vmu.io.dmem_req_vec.tag <> io.dmem_req_vec.bits.tag
-  //vmu.io.dmem_req_vec.valid <> io.dmem_req_vec.valid
-  //vmu.io.dmem_req_vec.rdy <> io.dmem_req_vec.ready
-  //vmu.io.dmem_resp_vec.valid <> io.dmem_resp_vec.valid
-
-  //vmu.io.dmem_req_ut.addr <> io.dmem_req_ut.bits.addr
-  //vmu.io.dmem_req_ut.op <> io.dmem_req_ut.bits.op
-  //vmu.io.dmem_req_ut.data <> io.dmem_req_ut.bits.data
-  //vmu.io.dmem_req_ut.wmask <> io.dmem_req_ut.bits.wmask
-  //vmu.io.dmem_req_ut.tag <> io.dmem_req_ut.bits.tag
-  //vmu.io.dmem_req_ut.valid <> io.dmem_req_ut.valid
-  //vmu.io.dmem_req_ut.rdy <> io.dmem_req_ut.ready
-  //vmu.io.dmem_resp_ut.valid <> io.dmem_resp_ut.valid
-
-  //vmu.io.dmem_resp_ut.tag <> io.dmem_resp_ut.bits.tag
-  //vmu.io.dmem_resp_ut.data <> io.dmem_resp_ut.bits.data
-  //vmu.io.dmem_resp_vec.tag <> io.dmem_resp_vec.bits.tag
-  //vmu.io.dmem_resp_vec.data <> io.dmem_resp_vec.bits.data
 }
