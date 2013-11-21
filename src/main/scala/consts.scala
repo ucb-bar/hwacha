339998d4e126a5bb07d1e6f5e3baefc6c1b3180c
Yunsup Lee <yunsup@cs.berkeley.edu>
2013-11-20 17:04:41 -0800

    refactor constants part2

--- src/main/scala/consts.scala
+++ src/main/scala/consts.scala
@@ -4,10 +4,36 @@ import Chisel._
 import Node._
 import uncore.constants.MemoryOpConstants._
 
-object Constants
-{
-  val SZ_VLENMAX  = 11
+object Constants extends
+  MachineConstants with
+  VectorCommandQueueConstants with
+  LaneConstants with
+  AIWConstants with
+  DecodeConstants with
+  VIUConstants with
+  VAU0Constants with
+  VAU1Constants with
+  VAU2Constants with
+  VMUConstants with
+  PVFBConstants
 
+trait MachineConstants
+{
+  val SZ_VLENMAX = 11
+  val SZ_VLEN = 11
+  val SZ_REGLEN = 6
+  val SZ_REGCNT = 6
+
+  val SZ_ADDR = 64
+  val SZ_INST = 32
+  val SZ_DATA = 65
+  val SZ_EXC = 5
+  val SZ_XLEN = 64
+  val SZ_FLEN = 65
+}
+
+trait VectorCommandQueueConstants
+{
   val SZ_VCMD = 20
   val SZ_VCMD_CMD = 8
   val SZ_VCMD_VD = 6
@@ -26,32 +52,18 @@ object Constants
   val RG_XIMM1_BACTIVE = (30,23)
   val RG_XIMM1_BCNT = (34,31)
 
-  val SZ_VIMM      = 64
-  val SZ_XIMM      = 64
+  val SZ_VIMM = 64
+  val SZ_XIMM = 64
 
-  val SZ_VSTRIDE   = 64
-  val SZ_XIMM2     = 64
+  val SZ_VSTRIDE = 64
+  val SZ_XIMM2 = 64
 
-  val SZ_VRESP     = 32
-  val SZ_XRESP     = 1
+  val SZ_VRESP = 32
+  val SZ_XRESP = 1
+}
 
-  val SZ_ADDR = 64
-  val SZ_INST = 32
-  val SZ_DATA = 65
-  val SZ_EXC = 5
-  val SZ_XLEN = 64
-  val SZ_FLEN = 65
-
-  val SZ_STALL = 3
-
-  val RG_VAQ = 2
-  val RG_VLDQ = 1
-  val RG_VSDQ = 0
-
-  val SZ_VLEN = 11
-  val SZ_REGLEN = 6
-  val SZ_REGCNT = 6
-
+trait LaneConstants
+{
   val SZ_BANK = 8
   val SZ_LGBANK = 3
   val SZ_LGBANK1 = 4
@@ -64,11 +76,10 @@ object Constants
   val SZ_BOPL = 2
   val SZ_BRPORT = SZ_BANK
   val SZ_BWPORT = 3
-  
-  val VACKCNT = 32
-  val VACKCNT_FX = UInt(VACKCNT)
-  val SZ_VACKCNT = log2Up(VACKCNT)
+}
 
+trait AIWConstants
+{
   val AIW_CMD_DEPTH = 8
   val AIW_IMM1_DEPTH = 8
   val AIW_IMM2_DEPTH = 8
@@ -80,10 +91,10 @@ object Constants
   val SZ_AIW_IMM2E = log2Up(AIW_IMM2_DEPTH)
   val SZ_AIW_CNT = log2Up(AIW_IMM2_DEPTH)
   val SZ_AIW_NUMCNT = log2Up(AIW_NUMCNT_DEPTH)
+}
 
-  val SZ_QCNT = SZ_LGBANK1
-
-  // constans for vau0, vau1, vau2
+trait DecodeConstants
+{
   val M0 = Bits("b00", 2)
   val MR = Bits("b01", 2)
   val ML = Bits("b10", 2)
@@ -106,6 +117,13 @@ object Constants
   val FPS = Bits("b0", 1)
   val FPD = Bits("b1", 1)
 
+  val MTF_X = Bool(false)
+  val MTF_N = Bool(false)
+  val MTF_Y = Bool(true)
+}
+
+trait VIUConstants
+{
   val ENUM_VIU = 28
   val viu_ADD :: viu_SLL :: viu_SLT :: viu_SLTU :: viu_XOR :: viu_SRL :: viu_SRA :: viu_OR :: viu_AND :: viu_SUB :: viu_IDX :: viu_MOV :: viu_FP_BRANCH = Enum( UInt(), ENUM_VIU)
   val viu_FSJ :: viu_FSJN :: viu_FSJX :: viu_FEQ :: viu_FLT :: viu_FLE :: viu_FMIN  :: viu_FMAX :: viu_MOVZ :: viu_MOVN :: viu_BRANCH = viu_FP_BRANCH
@@ -120,29 +138,46 @@ object Constants
 
   def isVIUBranch(viu: UInt) = viu >= viu_BNE && viu <= viu_BGEU
 
-  // in the decode table
+  val SZ_VBR_FN = 11
+  val SZ_VIU_FN = 11
+
+  val RG_VIU_T = (SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW+SZ_VIU_T-1           , SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW)
+  val RG_VIU_T0 = (SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW+SZ_VIU_T1+SZ_VIU_T0-1, SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW+SZ_VIU_T1)
+  val RG_VIU_T1 = (SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW+SZ_VIU_T1-1          , SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW)
+  val RG_VIU_DW = (SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW-1                    , SZ_VIU_OP+SZ_VIU_FP) 
+  val RG_VIU_FP = (SZ_VIU_OP+SZ_VIU_FP-1                              , SZ_VIU_OP)
+  val RG_VIU_FN = (SZ_VIU_OP-1,0)
+}
+
+trait VAU0Constants extends DecodeConstants
+{
   val ENUM_VAU0 = 4
   val vau0_M :: vau0_MH :: vau0_MHSU :: vau0_MHU :: Nil = Enum( UInt(), ENUM_VAU0 )
   val SZ_VAU0 = vau0_M.getWidth
   val vau0_X = Bits(0, SZ_VAU0)
 
   // acutal ops
-  val VAU0_32    = Cat(DW32,vau0_M)
-  val VAU0_32H   = Cat(DW32,vau0_MH)
+  val VAU0_32 = Cat(DW32,vau0_M)
+  val VAU0_32H = Cat(DW32,vau0_MH)
   val VAU0_32HSU = Cat(DW32,vau0_MHSU)
-  val VAU0_32HU  = Cat(DW32,vau0_MHU)
-  val VAU0_64    = Cat(DW64,vau0_M)
-  val VAU0_64H   = Cat(DW64,vau0_MH)
+  val VAU0_32HU = Cat(DW32,vau0_MHU)
+  val VAU0_64 = Cat(DW64,vau0_M)
+  val VAU0_64H = Cat(DW64,vau0_MH)
   val VAU0_64HSU = Cat(DW64,vau0_MHSU)
-  val VAU0_64HU  = Cat(DW64,vau0_MHU)
+  val VAU0_64HU = Cat(DW64,vau0_MHU)
 
+  val SZ_VAU0_FN = 3
+}
+
+trait VAU1Constants
+{
   // Can't use enums for VAU1 because decode depends on the top bit
-  val VAU1_X     = UInt(0, 3)
-  val VAU1_ADD   = UInt(0, 3)
-  val VAU1_SUB   = UInt(1, 3)
-  val VAU1_MUL   = UInt(2, 3)
-  val VAU1_MADD  = UInt(4, 3)
-  val VAU1_MSUB  = UInt(5, 3)
+  val VAU1_X = UInt(0, 3)
+  val VAU1_ADD = UInt(0, 3)
+  val VAU1_SUB = UInt(1, 3)
+  val VAU1_MUL = UInt(2, 3)
+  val VAU1_MADD = UInt(4, 3)
+  val VAU1_MSUB = UInt(5, 3)
   val VAU1_NMSUB = UInt(6, 3)
   val VAU1_NMADD = UInt(7, 3)
 
@@ -151,38 +186,31 @@ object Constants
   // val SZ_VAU1 = vau1_ADD.getWidth
   // val vau1_X = Bits(0, SZ_VAU1)
 
-  val ENUM_VAU2 = 12
-  val vau2_CLTF :: vau2_CLUTF :: vau2_CWTF :: vau2_CWUTF :: vau2_MXTF :: vau2_CFTL :: vau2_CFTLU :: vau2_CFTW :: vau2_CFTWU :: vau2_MFTX :: vau2_CDTS :: vau2_CSTD :: Nil = Enum( UInt(), ENUM_VAU2 )
-  val SZ_VAU2 = vau2_CLTF.getWidth
-  val vau2_X = Bits(0, SZ_VAU2)
-
-  val SZ_VBR_FN = 11
-  val SZ_VIU_FN  = 11
-  val SZ_VAU0_FN = 3
   val SZ_VAU1_FN = 7
-  val SZ_VAU2_FN = 8
-
-  val RG_VIU_T  = (SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW+SZ_VIU_T-1           , SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW)
-  val RG_VIU_T0 = (SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW+SZ_VIU_T1+SZ_VIU_T0-1, SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW+SZ_VIU_T1)
-  val RG_VIU_T1 = (SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW+SZ_VIU_T1-1          , SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW)
-  val RG_VIU_DW = (SZ_VIU_OP+SZ_VIU_FP+SZ_VIU_DW-1                    , SZ_VIU_OP+SZ_VIU_FP) 
-  val RG_VIU_FP = (SZ_VIU_OP+SZ_VIU_FP-1                              , SZ_VIU_OP)
-  val RG_VIU_FN = (SZ_VIU_OP-1,0)
 
   val RG_VAU1_FP = 6
   val RG_VAU1_RM = (5,3)
   val RG_VAU1_FN = (2,0)
 
   val FN_VAU1_FMA = (x: Bits) => x(2)
+}
+
+trait VAU2Constants
+{
+  val ENUM_VAU2 = 12
+  val vau2_CLTF :: vau2_CLUTF :: vau2_CWTF :: vau2_CWUTF :: vau2_MXTF :: vau2_CFTL :: vau2_CFTLU :: vau2_CFTW :: vau2_CFTWU :: vau2_MFTX :: vau2_CDTS :: vau2_CSTD :: Nil = Enum( UInt(), ENUM_VAU2 )
+  val SZ_VAU2 = vau2_CLTF.getWidth
+  val vau2_X = Bits(0, SZ_VAU2)
+
+  val SZ_VAU2_FN = 8
 
   val RG_VAU2_FP = 7
   val RG_VAU2_RM = (6,4)
   val RG_VAU2_FN = (SZ_VAU2-1,0)
+}
 
-  val MTF_X = Bool(false)
-  val MTF_N = Bool(false)
-  val MTF_Y = Bool(true)
-
+trait VMUConstants extends LaneConstants
+{
   def is_mtype_byte(typ: UInt) = (typ === MT_B || typ === MT_BU)
   def is_mtype_halfword(typ: UInt) = (typ === MT_H || typ === MT_HU)
   def is_mtype_word(typ: UInt) = (typ === MT_W || typ === MT_WU)
@@ -195,7 +223,11 @@ object Constants
   def is_mcmd_pfw(cmd: UInt) = (cmd === M_PFW)
   def is_mcmd_pf(cmd: UInt) = (is_mcmd_pfr(cmd) || is_mcmd_pfw(cmd))
 
-  // PVFB Constants
+  val SZ_QCNT = SZ_LGBANK1
+}
+
+trait PVFBConstants extends LaneConstants
+{
   var coarseGrained = false
   var HAVE_PVFB = false
   var NUM_PVFB = 4
@@ -209,99 +241,99 @@ object Constants
   def SZ_BMASK = log2Up(WIDTH_BMASK)
 }
 
-object Commands
+object Commands extends Commands
+trait Commands
 {
   // command bits for the vector command queue
-  val CMD_X          = Bits(0,8)
+  val CMD_X = Bits(0,8)
 
-  val CMD_VVCFGIVL   = Bits("b00_0000_00",8)
-  val CMD_VSETVL     = Bits("b00_0000_10",8)
-  val CMD_VF         = Bits("b00_0000_11",8)
+  val CMD_VVCFGIVL = Bits("b00_0000_00",8)
+  val CMD_VSETVL = Bits("b00_0000_10",8)
+  val CMD_VF = Bits("b00_0000_11",8)
 
-  val CMD_FENCE_L_V  = Bits("b00_001_100",8)
-  val CMD_FENCE_G_V  = Bits("b00_001_101",8)
+  val CMD_FENCE_L_V = Bits("b00_001_100",8)
+  val CMD_FENCE_G_V = Bits("b00_001_101",8)
   val CMD_FENCE_L_CV = Bits("b00_001_110",8)
   val CMD_FENCE_G_CV = Bits("b00_001_111",8)
 
-  val CMD_LDWB       = Bits("b00_010_000",8)
-  val CMD_STAC       = Bits("b00_010_001",8)
+  val CMD_LDWB = Bits("b00_010_000",8)
+  val CMD_STAC = Bits("b00_010_001",8)
 
-  val CMD_VMVV       = Bits("b01_000_000",8)
-  val CMD_VMSV       = Bits("b01_001_000",8)
-  val CMD_VMST       = Bits("b01_010_000",8)
-  val CMD_VMTS       = Bits("b01_011_000",8)
-  val CMD_VFMVV      = Bits("b01_000_001",8)
-  val CMD_VFMSV      = Bits("b01_001_001",8)
-  val CMD_VFMST      = Bits("b01_010_001",8)
-  val CMD_VFMTS      = Bits("b01_011_001",8)
+  val CMD_VMVV = Bits("b01_000_000",8)
+  val CMD_VMSV = Bits("b01_001_000",8)
+  val CMD_VMST = Bits("b01_010_000",8)
+  val CMD_VMTS = Bits("b01_011_000",8)
+  val CMD_VFMVV = Bits("b01_000_001",8)
+  val CMD_VFMSV = Bits("b01_001_001",8)
+  val CMD_VFMST = Bits("b01_010_001",8)
+  val CMD_VFMTS = Bits("b01_011_001",8)
 
-  val CMD_VLD        = Bits("b1_00_0_0_0_11",8)
-  val CMD_VLW        = Bits("b1_00_0_0_0_10",8)
-  val CMD_VLWU       = Bits("b1_00_0_0_1_10",8)
-  val CMD_VLH        = Bits("b1_00_0_0_0_01",8)
-  val CMD_VLHU       = Bits("b1_00_0_0_1_01",8)
-  val CMD_VLB        = Bits("b1_00_0_0_0_00",8)
-  val CMD_VLBU       = Bits("b1_00_0_0_1_00",8)
-  val CMD_VSD        = Bits("b1_00_1_0_0_11",8)
-  val CMD_VSW        = Bits("b1_00_1_0_0_10",8)
-  val CMD_VSH        = Bits("b1_00_1_0_0_01",8)
-  val CMD_VSB        = Bits("b1_00_1_0_0_00",8)
+  val CMD_VLD = Bits("b1_00_0_0_0_11",8)
+  val CMD_VLW = Bits("b1_00_0_0_0_10",8)
+  val CMD_VLWU = Bits("b1_00_0_0_1_10",8)
+  val CMD_VLH = Bits("b1_00_0_0_0_01",8)
+  val CMD_VLHU = Bits("b1_00_0_0_1_01",8)
+  val CMD_VLB = Bits("b1_00_0_0_0_00",8)
+  val CMD_VLBU = Bits("b1_00_0_0_1_00",8)
+  val CMD_VSD = Bits("b1_00_1_0_0_11",8)
+  val CMD_VSW = Bits("b1_00_1_0_0_10",8)
+  val CMD_VSH = Bits("b1_00_1_0_0_01",8)
+  val CMD_VSB = Bits("b1_00_1_0_0_00",8)
 
-  val CMD_VFLD       = Bits("b1_00_0_1_0_11",8)
-  val CMD_VFLW       = Bits("b1_00_0_1_0_10",8)
-  val CMD_VFSD       = Bits("b1_00_1_1_0_11",8)
-  val CMD_VFSW       = Bits("b1_00_1_1_0_10",8)
+  val CMD_VFLD = Bits("b1_00_0_1_0_11",8)
+  val CMD_VFLW = Bits("b1_00_0_1_0_10",8)
+  val CMD_VFSD = Bits("b1_00_1_1_0_11",8)
+  val CMD_VFSW = Bits("b1_00_1_1_0_10",8)
 
-  val CMD_VLSTD      = Bits("b1_01_0_0_0_11",8)
-  val CMD_VLSTW      = Bits("b1_01_0_0_0_10",8)
-  val CMD_VLSTWU     = Bits("b1_01_0_0_1_10",8)
-  val CMD_VLSTH      = Bits("b1_01_0_0_0_01",8)
-  val CMD_VLSTHU     = Bits("b1_01_0_0_1_01",8)
-  val CMD_VLSTB      = Bits("b1_01_0_0_0_00",8)
-  val CMD_VLSTBU     = Bits("b1_01_0_0_1_00",8)
-  val CMD_VSSTD      = Bits("b1_01_1_0_0_11",8)
-  val CMD_VSSTW      = Bits("b1_01_1_0_0_10",8)
-  val CMD_VSSTH      = Bits("b1_01_1_0_0_01",8)
-  val CMD_VSSTB      = Bits("b1_01_1_0_0_00",8)
+  val CMD_VLSTD = Bits("b1_01_0_0_0_11",8)
+  val CMD_VLSTW = Bits("b1_01_0_0_0_10",8)
+  val CMD_VLSTWU = Bits("b1_01_0_0_1_10",8)
+  val CMD_VLSTH = Bits("b1_01_0_0_0_01",8)
+  val CMD_VLSTHU = Bits("b1_01_0_0_1_01",8)
+  val CMD_VLSTB = Bits("b1_01_0_0_0_00",8)
+  val CMD_VLSTBU = Bits("b1_01_0_0_1_00",8)
+  val CMD_VSSTD = Bits("b1_01_1_0_0_11",8)
+  val CMD_VSSTW = Bits("b1_01_1_0_0_10",8)
+  val CMD_VSSTH = Bits("b1_01_1_0_0_01",8)
+  val CMD_VSSTB = Bits("b1_01_1_0_0_00",8)
 
-  val CMD_VFLSTD     = Bits("b1_01_0_1_0_11",8)
-  val CMD_VFLSTW     = Bits("b1_01_0_1_0_10",8)
-  val CMD_VFSSTD     = Bits("b1_01_1_1_0_11",8)
-  val CMD_VFSSTW     = Bits("b1_01_1_1_0_10",8)
+  val CMD_VFLSTD = Bits("b1_01_0_1_0_11",8)
+  val CMD_VFLSTW = Bits("b1_01_0_1_0_10",8)
+  val CMD_VFSSTD = Bits("b1_01_1_1_0_11",8)
+  val CMD_VFSSTW = Bits("b1_01_1_1_0_10",8)
 
-  val CMD_VLXD       = Bits("b1_10_0_0_0_11",8)
-  val CMD_VLXW       = Bits("b1_10_0_0_0_10",8)
-  val CMD_VLXWU      = Bits("b1_10_0_0_1_10",8)
-  val CMD_VLXH       = Bits("b1_10_0_0_0_01",8)
-  val CMD_VLXHU      = Bits("b1_10_0_0_1_01",8)
-  val CMD_VLXB       = Bits("b1_10_0_0_0_00",8)
-  val CMD_VLXBU      = Bits("b1_10_0_0_1_00",8)
-  val CMD_VSXD       = Bits("b1_10_1_0_0_11",8)
-  val CMD_VSXW       = Bits("b1_10_1_0_0_10",8)
-  val CMD_VSXH       = Bits("b1_10_1_0_0_01",8)
-  val CMD_VSXB       = Bits("b1_10_1_0_0_00",8)
+  val CMD_VLXD = Bits("b1_10_0_0_0_11",8)
+  val CMD_VLXW = Bits("b1_10_0_0_0_10",8)
+  val CMD_VLXWU = Bits("b1_10_0_0_1_10",8)
+  val CMD_VLXH = Bits("b1_10_0_0_0_01",8)
+  val CMD_VLXHU = Bits("b1_10_0_0_1_01",8)
+  val CMD_VLXB = Bits("b1_10_0_0_0_00",8)
+  val CMD_VLXBU = Bits("b1_10_0_0_1_00",8)
+  val CMD_VSXD = Bits("b1_10_1_0_0_11",8)
+  val CMD_VSXW = Bits("b1_10_1_0_0_10",8)
+  val CMD_VSXH = Bits("b1_10_1_0_0_01",8)
+  val CMD_VSXB = Bits("b1_10_1_0_0_00",8)
 
-  val CMD_VFLXD      = Bits("b1_10_0_1_0_11",8)
-  val CMD_VFLXW      = Bits("b1_10_0_1_0_10",8)
-  val CMD_VFSXD      = Bits("b1_10_1_1_0_11",8)
-  val CMD_VFSXW      = Bits("b1_10_1_1_0_10",8)
+  val CMD_VFLXD = Bits("b1_10_0_1_0_11",8)
+  val CMD_VFLXW = Bits("b1_10_0_1_0_10",8)
+  val CMD_VFSXD = Bits("b1_10_1_1_0_11",8)
+  val CMD_VFSXW = Bits("b1_10_1_1_0_10",8)
 
-  val CMD_VAMOADDD   = Bits("b1_11_000_11",8)
-  val CMD_VAMOSWAPD  = Bits("b1_11_001_11",8)
-  val CMD_VAMOANDD   = Bits("b1_11_010_11",8)
-  val CMD_VAMOORD    = Bits("b1_11_011_11",8)
-  val CMD_VAMOMIND   = Bits("b1_11_100_11",8)
-  val CMD_VAMOMAXD   = Bits("b1_11_101_11",8)
-  val CMD_VAMOMINUD  = Bits("b1_11_110_11",8)
-  val CMD_VAMOMAXUD  = Bits("b1_11_111_11",8)
+  val CMD_VAMOADDD = Bits("b1_11_000_11",8)
+  val CMD_VAMOSWAPD = Bits("b1_11_001_11",8)
+  val CMD_VAMOANDD = Bits("b1_11_010_11",8)
+  val CMD_VAMOORD = Bits("b1_11_011_11",8)
+  val CMD_VAMOMIND = Bits("b1_11_100_11",8)
+  val CMD_VAMOMAXD = Bits("b1_11_101_11",8)
+  val CMD_VAMOMINUD = Bits("b1_11_110_11",8)
+  val CMD_VAMOMAXUD = Bits("b1_11_111_11",8)
 
-  val CMD_VAMOADDW   = Bits("b1_11_000_10",8)
-  val CMD_VAMOSWAPW  = Bits("b1_11_001_10",8)
-  val CMD_VAMOANDW   = Bits("b1_11_010_10",8)
-  val CMD_VAMOORW    = Bits("b1_11_011_10",8)
-  val CMD_VAMOMINW   = Bits("b1_11_100_10",8)
-  val CMD_VAMOMAXW   = Bits("b1_11_101_10",8)
-  val CMD_VAMOMINUW  = Bits("b1_11_110_10",8)
-  val CMD_VAMOMAXUW  = Bits("b1_11_111_10",8)
-
+  val CMD_VAMOADDW = Bits("b1_11_000_10",8)
+  val CMD_VAMOSWAPW = Bits("b1_11_001_10",8)
+  val CMD_VAMOANDW = Bits("b1_11_010_10",8)
+  val CMD_VAMOORW = Bits("b1_11_011_10",8)
+  val CMD_VAMOMINW = Bits("b1_11_100_10",8)
+  val CMD_VAMOMAXW = Bits("b1_11_101_10",8)
+  val CMD_VAMOMINUW = Bits("b1_11_110_10",8)
+  val CMD_VAMOMAXUW = Bits("b1_11_111_10",8)
 }
