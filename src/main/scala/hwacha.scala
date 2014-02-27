b0b6f02c0058cbb8bd65432bd8a7039c901c2717
Yunsup Lee <yunsup@cs.berkeley.edu>
2014-02-27 04:32:33 -0800

    commit vru = true

--- src/main/scala/hwacha.scala
+++ src/main/scala/hwacha.scala
@@ -7,7 +7,7 @@ import Constants._
 case class HwachaConfiguration(vicache: rocket.ICacheConfig, dcache: rocket.DCacheConfig, nbanks: Int, nreg_per_bank: Int, ndtlb: Int, nptlb: Int)
 {
   val nreg_total = nbanks * nreg_per_bank
-  val vru = false
+  val vru = true
   val confprec = false
 
   // rocket pipeline latencies
