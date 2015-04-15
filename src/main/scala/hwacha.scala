6b5b7b272e34242a68a5c31cd236f2651967c796
Yunsup Lee <yunsup@cs.berkeley.edu>
2015-04-14 23:38:03 -0700

    remove vestige code

--- src/main/scala/hwacha.scala
+++ src/main/scala/hwacha.scala
@@ -54,22 +54,6 @@ abstract trait UsesHwachaParameters extends UsesParameters {
   val fconv_stages = 3
   val fcmp_stages = 1
 
-  val second_fma_pipe = true
-  val second_fconv_pipe = false
-
-  val delay_seq_exp = 2
-
-  val ptr_incr_max =
-    nbanks-1 +
-    List(int_stages+2, imul_stages+3, fma_stages+4, fconv_stages+2).reduce(scala.math.max(_,_)) +
-    delay_seq_exp +
-    2 // buffer
-  val ptr_incr_sz = log2Up(ptr_incr_max)
-
-  val shift_buf_read = 3
-  val shift_buf_write =
-    List(imul_stages+3, fma_stages+4, fconv_stages+2).reduce(scala.math.max(_,_)) + 1
-
 
   val confvcmdq = new {
     val ncmd = 16
