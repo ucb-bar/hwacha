61292784f30adeee1b896b5e5bd04c888ab15752
Albert Ou <aou@eecs.berkeley.edu>
2015-10-27 16:32:38 -0700

    Genericize VLT implementation

--- src/main/scala/vmu-ldata.scala
+++ src/main/scala/vmu-ldata.scala
@@ -3,46 +3,49 @@ package hwacha
 import Chisel._
 import cde.Parameters
 
-class VLTWIO(implicit val p: Parameters) extends DecoupledIO(new VLTEntry()(p)) with VMUParameters {
-  val tag = UInt(INPUT, bTag)
+class TableWIO[T <: Data](gen: => T, sztag: Int)
+  extends DecoupledIO(gen) {
+  val tag = UInt(INPUT, sztag)
 }
 
-class VLTRIO(implicit p: Parameters) extends ValidIO(new VMUBundle()(p) with VMUTag) {
-  val meta = new VLTEntry().asInput()
+class TableRIO[T <: Data](gen: => T, sztag: Int)
+  extends ValidIO(UInt(width = sztag)) {
+  val record = gen.asInput()
+}
+
+class Table[T <: Data](n: Int, gen: => T) extends Module {
+  private val sztag = log2Up(n)
+  val io = new Bundle {
+    val r = new TableRIO(gen, sztag).flip
+    val w = new TableWIO(gen, sztag).flip
+  }
+
+  val valid = Reg(init = Bits(0, n))
+  val array = Mem(gen, n)
+
+  io.w.ready := !valid.andR
+
+  private val rtag = io.r.bits
+  private val wtag = io.w.tag
+  wtag := CTZ(~valid, n)
+
+  val wen = io.w.fire()
+  val ren = io.r.valid
+  val valid_mask_r = ren << rtag
+  val valid_mask_w = wen << wtag
+  valid := (valid & (~valid_mask_r)) | valid_mask_w
+
+  assert(!ren || valid(rtag), "table: invalid read tag")
+
+  io.r.record := array(rtag)
+  when (wen) {
+    array(io.w.tag) := io.w.bits
+  }
 }
 
 class VMLUIO(implicit p: Parameters) extends VMUBundle()(p) {
   val load = Decoupled(new VMLUData)
-  val meta = new VLTWIO
-}
-
-class VLT(implicit p: Parameters) extends VMUModule()(p) {
-  val io = new Bundle {
-    val r = new VLTRIO().flip
-    val w = new VLTWIO().flip
-  }
-
-  val valid = Reg(init = Bits(0, nVLT))
-  val table = Mem(new VLTEntry, nVLT)
-
-  io.w.ready := !valid.andR
-
-  private val rtag = io.r.bits.tag
-  private val wtag = io.w.tag
-  wtag := PriorityEncoder(~valid)
-
-  val wen = io.w.fire()
-  val ren = io.r.valid
-  val valid_mask_r = (Bits(1) << rtag) & Fill(nVLT, ren)
-  val valid_mask_w = (Bits(1) << wtag) & Fill(nVLT, wen)
-  valid := (valid & (~valid_mask_r)) | valid_mask_w
-
-  assert(!ren || valid(rtag), "VLT: invalid read tag")
-
-  io.r.meta := table(rtag)
-  when (wen) {
-    table(io.w.tag) := io.w.bits
-  }
+  val meta = new TableWIO(new VLTEntry, bTag)
 }
 
 class LBox(implicit p: Parameters)  extends VMUModule()(p) {
@@ -52,14 +55,14 @@ class LBox(implicit p: Parameters)  extends VMUModule()(p) {
   }
 
   val vldq = Module(new Queue(io.lane.bits, nVLDQ))
-  val vlt = Module(new VLT)
+  val vlt = Module(new Table(nVLT, new VLTEntry))
 
   vlt.io.w <> io.mem.meta
-  vlt.io.r.bits.tag := io.mem.load.bits.tag
+  vlt.io.r.bits := io.mem.load.bits.tag
   vlt.io.r.valid := io.mem.load.fire()
 
   vldq.io.enq.bits.data := io.mem.load.bits.data
-  vldq.io.enq.bits.meta := vlt.io.r.meta
+  vldq.io.enq.bits.meta := vlt.io.r.record
   vldq.io.enq.bits.meta.last := io.mem.load.bits.last
   vldq.io.enq.valid := io.mem.load.valid
   io.mem.load.ready := vldq.io.enq.ready
